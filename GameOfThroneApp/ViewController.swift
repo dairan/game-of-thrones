1462e254a6f686b1f24a2b3cb144176228518f9c
Dairan <dairan@dairan.com>
2022-08-07 17:32:48 -0300

    Initial Commit

---
+++ GameOfThroneApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GameOfThroneApp
+//
+//  Created by Dairan on 07/08/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
